08dfdc2e525aa5c7adabd74a28c385c09f839043
Vihas Shah <vihasshah@gmail.com>
2018-09-23 01:35:03 +0530

    search by place and category added

--- core/Cottage.php
+++ core/Cottage.php
@@ -32,8 +32,38 @@
                 $this->helper->create_response(false,"List Not Found",null);
             }
         }
+
+        /**
+         * strict search in category id and lasy search in place
+         * @param searchInput input value by user
+         * @param field identifier
+         *      category -> search by category
+         *      place -> search by place
+         */
+        function search_by($searchInput,$field){
+            $query = "SELECT * FROM cottages"; // all data (filter not applied) 
+            if($field == 'category'){
+                $query = "SELECT * FROM cottages where category_id='$searchInput'"; // search by category
+            }else if($field == 'place'){
+                $query = "SELECT * FROM cottages where place LIKE '$searchInput'"; // search by place (lasy search)
+            }
+            $res = mysqli_query($this->conn,$query);
+            if(mysqli_num_rows($res) > 0){
+                $list = [];
+                while($result = mysqli_fetch_array($res,MYSQLI_ASSOC)){
+                    $list[] = $result;
+                }
+                $this->helper->create_response(true,"Found",$list);
+            }else{
+                $this->helper->create_response(false,"No Results Found",null);
+            }
+        }
+
+        
     }
 
     $cottage = new Cottage();
-    $cottage->get_list();
+    // $cottage->get_list();
+    $cottage->search_by("%ahmedabad%",'place');
+    // $cottage->search_by("1",'category');
 ?>
