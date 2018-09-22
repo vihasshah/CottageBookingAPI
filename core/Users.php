f935220382920f2993b78ffcc50b4ec59ba3bb8f
Vihas Shah <vihasshah@gmail.com>
2018-09-23 00:10:06 +0530

    user info update added

--- core/Users.php
+++ core/Users.php
@@ -21,7 +21,6 @@
         function user_exist($email){
             $query = "select * from users where email='$email'";
             $res = mysqli_query($this->conn,$query);
-            print_r($res);
             if(mysqli_num_rows($res) > 0){
                 return true;
             }else{
@@ -48,9 +47,24 @@
                 $this->helper->create_response(false,"User already registered",null);
             }
         }
+
+        function update_user_info($data){
+            $id = $data['user_id'];
+            $firstname = $data['firstname'];
+            $lastname = $data['lastname'];
+            $contact = $data['contact'];
+            $email = $data['email'];
+            $query = "update users set firstname='$firstname', lastname='$lastname', contact='$contact', email='$email' where id='$id'";
+            $res = mysqli_query($this->conn,$query);
+            if(mysqli_affected_rows($this->conn) > 0){
+                $this->helper->create_response(true,"User Info update",null);
+            }else{
+                $this->helper->create_response(false,"User Info not updated",null);
+            }
+        }
     }
 
     $users = new Users();
-    $users->add_user(array("firstname"=>"testing","lastname"=>"testing","contact"=>"123","email"=>"email@email.com","password"=>"password"));
+    $users->update_user_info(array("firstname"=>"ula","lastname"=>"testing","contact"=>"123","email"=>"email@email.com","user_id"=>4));
     $users->user_exist("email@email.com");
 ?>
