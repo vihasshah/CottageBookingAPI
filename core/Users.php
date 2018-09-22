9aec870127f56a0b1032d6a4022731b597f82898
Vihas Shah <vihasshah@gmail.com>
2018-09-23 00:35:44 +0530

    user authentication method added

--- core/Users.php
+++ core/Users.php
@@ -18,7 +18,7 @@
             print_r($this->conn);
         }
 
-        function user_exist($email){
+        function exist($email){
             $query = "select * from users where email='$email'";
             $res = mysqli_query($this->conn,$query);
             if(mysqli_num_rows($res) > 0){
@@ -28,14 +28,14 @@
             }
         }
 
-        function add_user($data){
+        function add($data){
             $firstname = $data['firstname'];
             $lastname = $data['lastname'];
             $contact = $data['contact'];
             $email = $data['email'];
-            $password = $data['password'];
+            $password = md5($data['password']);
 
-            if(!$this->user_exist($email)){
+            if(!$this->exist($email)){
                 $query = "insert into users values(null,'$firstname','$lastname','$contact','$email','$password')";
                 $res = mysqli_query($this->conn,$query);
                 if($res > 0){
@@ -48,23 +48,55 @@
             }
         }
 
-        function update_user_info($data){
-            $id = $data['user_id'];
+        function update_info($data){
+            $user_id = $data['user_id'];
             $firstname = $data['firstname'];
             $lastname = $data['lastname'];
             $contact = $data['contact'];
             $email = $data['email'];
-            $query = "update users set firstname='$firstname', lastname='$lastname', contact='$contact', email='$email' where id='$id'";
+
+            $query = "update users set firstname='$firstname', lastname='$lastname', contact='$contact', email='$email' where id='$user_id'";
             $res = mysqli_query($this->conn,$query);
             if(mysqli_affected_rows($this->conn) > 0){
-                $this->helper->create_response(true,"User Info update",null);
+                $this->helper->create_response(true,"User Info updated",null);
             }else{
                 $this->helper->create_response(false,"User Info not updated",null);
+            }
+        }
+
+        function update_password($data){
+            $email = $data['email'];
+            $newPassword = md5($data['password']);
+            $user_id = $data['user_id'];
+
+            $query = "update users set password='$newPassword' where email='$email' and id='$user_id'";
+            $res = mysqli_query($this->conn,$query);
+            if(mysqli_affected_rows($this->conn) > 0){
+                $this->helper->create_response(true,"Password updated",null);
+            }else{
+                $this->helper->create_response(false,"Same password as previous one",null);
+            }
+        }
+
+        function authenticate($data){
+            $email = $data['email'];
+            $password = md5($data['password']);
+            
+            $query = "select * from users where email='$email' and password='$password' limit 1";
+            $res = mysqli_query($this->conn,$query);
+
+            if(mysqli_num_rows($res) == 1){
+                $user_info = mysqli_fetch_array($res,MYSQLI_ASSOC);
+                unset($user_info['password']); // remove password key from array
+                $this->helper->create_response(true,"Authentication successful",$user_info);
+            }else{
+                $this->helper->create_response(false,"Invalid User",null);
             }
         }
     }
 
     $users = new Users();
-    $users->update_user_info(array("firstname"=>"ula","lastname"=>"testing","contact"=>"123","email"=>"email@email.com","user_id"=>4));
-    $users->user_exist("email@email.com");
+    $users->update_info(array("firstname"=>"ula","lastname"=>"testing","contact"=>"123","email"=>"email@email.com","user_id"=>4));
+    $users->update_password(array("password"=>"testPassword","email"=>"email@email.com","user_id"=>4));
+    $users->authenticate(array("password"=>"testPassword","email"=>"email@email.com"));
 ?>
