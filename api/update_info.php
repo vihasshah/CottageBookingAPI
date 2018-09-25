3282167f3b9c8db34bb02e8eeb5dcc9cf67cf8fd
Vihas Shah <vihasshah@gmail.com>
2018-09-25 11:00:34 +0530

    update user info endpoint created

---
+++ api/update_info.php
@@ -0,0 +1,22 @@
+<?php
+    include '../core/Users.php';
+    header("Accept:Application/JSON");
+    header("Content-Type:Application/JSON");
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $users = new Users();
+        $json = json_decode(file_get_contents('php://input'),TRUE);
+        $user_id = $json['user_id'];
+        $firstname = $json['firstname'];
+        $lastname = $json['lastname'];
+        $contact = $json['contact'];
+        $email = $json['email'];
+        $password = $json['password'];
+        $users->update_info(array(
+            'firstname'=>$firstname,
+            'lastname'=>$lastname,
+            'contact'=>$contact,
+            'email'=>$email,
+            "user_id"=>$user_id
+        ));
+    }
+?>
