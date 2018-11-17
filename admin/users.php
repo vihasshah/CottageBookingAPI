c3b7d2c491dcea6590570b59a6601c43c5090933
Vihas Shah <vihasshah@gmail.com>
2018-11-17 16:46:57 +0530

    users page created and navbar added

---
+++ admin/users.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <!-- color palette -->
+    <link rel="stylesheet" href="palette.css">
+    <!-- materialize -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    
+
+</head>
+<body>
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    
+    <!-- navbar -->
+    <nav>
+        <div class="nav-wrapper default-primary-color">
+            <a href="#!" class="brand-logo ">Cottage Finder</a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="index.php"><i class="material-icons left">store</i>Cottages</a></li>
+                <li><a href="users.php"><i class="material-icons left">person</i>Users</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
