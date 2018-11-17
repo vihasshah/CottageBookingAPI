56c0fea44e9a41a7618d3609fa2cc74aa66a4e06
Vihas Shah <vihasshah@gmail.com>
2018-11-17 18:47:34 +0530

    add cottage form completed

---
+++ admin/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.modal').modal(); // bottom sheet
+    M.updateTextFields(); // textfields
+    $('select').formSelect(); // dropdown menu
+    // $("#fab").click(() => {
+    //     $('#modal1').model('open')
+    // })
+    
+});
